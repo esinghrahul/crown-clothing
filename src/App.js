7dbf56d9f775d179f08057ce83cc9f2fc11f1fd5
esinghrahul <e.singh.rahul@outlook.com>
2020-06-08 19:17:21 +0530

    Initial Project Setup and homecomponent creation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './HomePage.component'
+import './HomePage.styles.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
