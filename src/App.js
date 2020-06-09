645c832c48e0eb01faa706624bad1d7cb902a3f0
esinghrahul <e.singh.rahul@outlook.com>
2020-06-09 17:57:13 +0530

    Added header and collection-item components along with styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/HomePage/HomePage.component'
+import ShopPage from './pages/shop/shop.component'
 import './pages/HomePage/HomePage.styles.scss'
+import Header from './components/header/header.component';
+
 
 function App() {
   return (
-      <HomePage />
+    <div>
+    <Header />
+    <Switch>
+      <Route exact= {true} path= '/' component={HomePage} />
+      <Route exact= {true} path= '/shop' component={ShopPage} />
+    </Switch>
+    </div>
   );
 }
 
--- src/components/menu-item/MenuItem.component.jsx
+++ src/components/menu-item/MenuItem.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 import './MenuItem.styles.scss'
 
-export const MenuItem = ({title, imageUrl, size}) => (
-    <div  className= {`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div  className= {`${size} menu-item`} onClick= {() => history.push(`${match.url}${linkUrl}`)}>
     <div className= 'background-image' style= {{backgroundImage: `url(${imageUrl})`}}></div>
         <div className="content">
             <h1 className= 'title'>{title.toUpperCase()}</h1>
@@ -10,3 +11,5 @@ export const MenuItem = ({title, imageUrl, size}) => (
         </div>
       </div>
 )
+
+export default withRouter(MenuItem)
