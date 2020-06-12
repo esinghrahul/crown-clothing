32afc58fb7f225862845a7913b91c1c5330b2760
esinghrahul <e.singh.rahul@outlook.com>
2020-06-13 00:24:01 +0530

    implemented styled components for custombutton, header and homepage components

--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Directory from '../../components/Directory/Directory.component'
+import {HomePageContainer} from './HomePage.styles'
 
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage
