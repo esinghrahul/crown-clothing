0bbe648a157618700276a0d623d1def4cdedc2ab
esinghrahul <e.singh.rahul@outlook.com>
2020-06-14 00:55:49 +0530

    Moved the signup and sign in functionality to leverage redux-saga

--- src/App.js
+++ src/App.js
@@ -9,59 +9,52 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import './pages/HomePage/HomePage.styles.scss'
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils'
-import {setCurrentUser} from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selector';
+import { checkUserSession } from './redux/user/user.actions';
 
 class App extends React.Component {
-  
-  unsubscribeFromAuth = null
-  componentDidMount(){
-    const {setCurrentUser} = this.props
-    this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth => {
-      // this.setState({currentUser: user})
-      if(userAuth){
-        const userRef= await createUserProfileDocument(userAuth)
-        userRef.onSnapshot(snapShot => {
-          // console.log(snapShot)
-          setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          })
-        })
-      }else{
-        setCurrentUser(userAuth)
-        // addCollectionAndDocuments('collections', collectionsArray.map(({title, items})=> ({title, items})))
-      }
-      // console.log(user)
-    })
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
-  componentWillUnmount(){
-    this.unsubscribeFromAuth()
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
-  render(){
-  return (
-    <div>
-    <Header />
-    <Switch>
-      <Route exact= {true} path= '/' component={HomePage} />
-      <Route path= '/shop' component={ShopPage} />
-      <Route exact path= '/signin' render= {()=> this.props.currentUser? (<Redirect to= '/' />): (<SignInAndSignUpPage />)} />
-      <Route path= '/checkout' exact component={CheckoutPage} />
-    </Switch>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-})
+});
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+  checkUserSession: () => dispatch(checkUserSession())
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
